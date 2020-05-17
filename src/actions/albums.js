dd40629ff0fcee11afec1bc15f0517038f65efc8
Michael Quarne <mikedq1225@gmail.com>
2020-05-17 10:59:01 -0700

    changed an image source

--- src/actions/albums.js
+++ src/actions/albums.js
@@ -1,53 +1,54 @@
 import axios from "axios"
 
 export function getAlbums() {
-  return new Promise((resolve, reject) => {
-    axios
-      .get("/albums")
-      .then(resp => {
-        resolve(resp.data)
-      })
-      .catch(e => {
-        reject()
-      })
-  })
+	return new Promise((resolve, reject) => {
+		axios
+			.get("/albums")
+			.then((resp) => {
+				resolve(resp.data)
+			})
+			.catch((e) => {
+				reject()
+			})
+	})
 }
 
 export function getAlbum(id) {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`/albums/${id}?_embed=pictures`)
-      .then(resp => {
-        resolve(resp.data)
-      })
-      .catch(e => {
-        reject()
-      })
-  })
+	return new Promise((resolve, reject) => {
+		axios
+			.get(`/albums/${id}?_embed=pictures`)
+			.then((resp) => {
+				console.log(resp.data)
+				resolve(resp.data)
+			})
+			.catch((e) => {
+				reject()
+			})
+	})
 }
 
 export function getPhoto(id) {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`/pictures/${id}`)
-      .then(resp => {
-        resolve(resp.data)
-      })
-      .catch(e => {
-        reject()
-      })
-  })
+	return new Promise((resolve, reject) => {
+		axios
+			.get(`/pictures/${id}`)
+			.then((resp) => {
+				resolve(resp.data)
+			})
+			.catch((e) => {
+				reject()
+			})
+	})
 }
 
 export function getPhotos() {
-  return new Promise((resolve, reject) => {
-    axios
-      .get("/pictures")
-      .then(resp => {
-        resolve(resp.data)
-      })
-      .catch(e => {
-        reject()
-      })
-  })
+	return new Promise((resolve, reject) => {
+		axios
+			.get("/pictures")
+			.then((resp) => {
+				resolve(resp.data)
+			})
+			.catch((e) => {
+				reject()
+			})
+	})
 }
